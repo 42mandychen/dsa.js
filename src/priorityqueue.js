700a7f93b9b1d6092719922d923ca2e0d0f200bf
Mandy Chen <42mandychen@users.noreply.github.com>
2017-08-17 00:59:49 -0700

    Add priority queue + tests

---
+++ src/priorityqueue.js
@@ -0,0 +1,36 @@
+import {MaxHeap} from './heap';
+
+// Element in the priority queue has a value and priority.
+class Element {
+  constructor(value, priority) {
+    this._value = value;
+    this._priority = priority;
+  }
+
+  get priority() {
+    return this._priority;
+  }
+}
+
+// The implementation of a priority queue uses max heap.
+export class PriorityQueue extends MaxHeap {
+  constructor() {
+    super((element) => {return element.priority});
+  }
+
+  enqueue(value, priority) {
+    super.insert(new Element(value, priority));
+  }
+
+  // remove the element with the highest priority
+  dequeue() {
+    let ele = super.deleteMax();
+    console.log(`element to dequeue: ${JSON.stringify(ele)}`);
+    if (ele != null) {
+      return ele;
+    } else {
+      return null;
+    }
+  }
+
+}
